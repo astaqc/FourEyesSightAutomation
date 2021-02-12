51d0f52a3e24ef8e0f3dd131003cd5efa711f978
memetics19 <shreeda.bhat@yahoo.com>
2021-02-13 00:06:03 +0530

    cleared local storage

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,16 +1,21 @@
 // import { describe } from "mocha";
+// import { it } from "mocha";
 import config from "../fixtures/config.json";
 import selectors from "./Selectors/Selectors";
 // import Selectors from"./Selectors/Selectors"
 
-describe("UI-Login Test",function(){
+describe("Login Test",function(){
     const Selectors = new selectors()
-    it("Login with given credentuials",function(){
+    it("1. Login with given credentials - UI",function(){
         cy.visit(config.baseURL)
         Selectors.getInputElbyType("email").type(config.username)
         Selectors.getInputElbyType("password").type(config.password)
         Selectors.getButtonElbyType("submit").click()
         cy.location('pathname').should('eq',"/home")
         cy.title().should('eq', 'Analytics Dashboard - Four Eyes Insight')
+        cy.clearLocalStorage()
     })
+    // it("2. Intercepting the API",function(){ 
+
+    // }
 })
